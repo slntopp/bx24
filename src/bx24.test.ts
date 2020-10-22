4f9e7e061bc9aabd6c341f15dca4ba4da3efac20
Mikita Iwanowski <info@slnt-opp.xyz>
2020-10-23 01:36:40 +0200

    test

--- src/bx24.test.ts
+++ src/bx24.test.ts
@@ -10,4 +10,9 @@ describe('bx24', () => {
     expect(bx24.getAuth).toBeDefined();
     expect(typeof bx24.getAuth).toBe('function');
   });
+  it("must have method 'getLang'", () => {
+    const bx24 = new BX24();
+    expect(bx24.getLang).toBeDefined();
+    expect(typeof bx24.getLang).toBe('function');
+  });
 });
